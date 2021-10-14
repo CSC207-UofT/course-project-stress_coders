8ced01118dae3392cea835a53b346ed1a2e3c50a
Devan Srinivasan <devan.srinivasan@mail.utoronto.ca>
2021-10-14 00:12:52 -0700

    Added the interface Throwable and Collectable for phase0. Put them in an Interfaces directory for organizational purposes.

---
+++ src/main/java/Interfaces/Collectable.java
@@ -0,0 +1,8 @@
+/**
+ * This interface is for any game object that can be collected (i.e. a potion)
+ */
+interface Collectable {
+    /** This method collects an object
+     */
+    void collect() {}
+}

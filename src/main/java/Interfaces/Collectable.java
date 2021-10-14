6e1ca5a1f7ca88f5510c5fa22055dc316e4ca59a
Devan Srinivasan <devan.srinivasan@mail.utoronto.ca>
2021-10-14 00:19:01 -0700

    fixed errors regarding interface

--- src/main/java/Interfaces/Collectable.java
+++ src/main/java/Interfaces/Collectable.java
@@ -1,8 +1,10 @@
+package main.java.Interfaces;
+
 /**
  * This interface is for any game object that can be collected (i.e. a potion)
  */
 interface Collectable {
     /** This method collects an object
      */
-    void collect() {}
+    default void collect() {}
 }
--- src/main/java/Interfaces/Throwable.java
+++ src/main/java/Interfaces/Throwable.java
@@ -1,10 +1,11 @@
+package main.java.Interfaces;
+
 /**
  * This interface is for any game object that can be thrown (i.e. an axe)
  */
 interface Throwable {
-    /** This method throws the object a given distance
+     /** This method throws the object a given distance
      * @param distance integer quantity that describes how far obj should be thrown
      */
-    void throw(int distance) {
-    }
+     default void throwObj(int distance) {}
 }

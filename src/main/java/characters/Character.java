8f024f753f7040c24bbcee7a615ca2e6a7a8fe01
MichelleChernyi <michelle.chernyi@mail.utoronto.ca>
2021-10-15 21:49:00 -0400

    added documentation to player,enemy,and character classes

--- src/main/java/characters/Character.java
+++ src/main/java/characters/Character.java
@@ -4,42 +4,89 @@ import weapons.Weapon;
 import java.util.List;
 
 public abstract class Character {
+    /**
+     * abstarct character with inventory, health points, and weapon.
+     */
     private List<Item> inventory;
     private int healthPoints;
     private Weapon weapon;
 
+    /**
+     * Creates a new Character with default inventory, health points, and weapon.
+     */
     public Character(){
         this.inventory = null;
         this.healthPoints = 100;
         this.weapon = null;
     }
 
+    /**
+     * Creates a new Character.
+     *
+     * @param inventory            the list of items that Character has
+     * @param healthPoints         the number of health points of Character
+     * @param weapon               weapon of Character
+     */
     public Character(List<Item> inventory, int healthPoints, Weapon weapon){
         this.inventory = inventory;
         this.healthPoints = healthPoints;
         this.weapon = weapon;
 
     }
+
+    /**
+     * set the inventory of this Character.
+     *
+     * @param inventory  list of Items of this Character
+     */
     public void setInventory(List<Item> inventory){
         this.inventory = inventory;
     }
 
+    /**
+     * set the health points of this Character.
+     *
+     * @param healthPoints  the number of health points of Character
+     */
     public void setHealthPoints(int healthPoints){
         this.healthPoints = healthPoints;
     }
+
+    /**
+     * set the weapon of this Character.
+     *
+     * @param weapon       weapon of this Character
+     */
     public void setWeapon(Weapon weapon){
         this.weapon = weapon;
     }
+
+    /**
+     * return the inventory of this Character.
+     *
+     * @return list of Items
+     */
     public List<Item> getInventory(){
         return this.inventory;
     }
-
+    /**
+     * return the health points of this Character.
+     *
+     * @return int that is health points of this character
+     */
     public int getHealthPoints() {
         return this.healthPoints;
     }
 
+    /**
+     * return the weapon of this Character.
+     *
+     * @return Weapon of this character
+     */
     public Weapon getWeapon() {
         return this.weapon;
     }
 
 }
+
+
--- src/main/java/characters/Player.java
+++ src/main/java/characters/Player.java
@@ -3,34 +3,54 @@ import items.Item;
 import weapons.Weapon;
 import java.util.List;
 
-public class Player extends Character{
+public class Player extends Character {
+    /**
+     *  A Player subclass of Character with inventory, health points, and weapon.
+     */
 
-    //Constructs a Player with attributes inventory, healthPoints, and weapon
+    /**
+     * Constructs a Player with attributes inventory, healthPoints, and weapon
+     *
+     * @param inventory    the list of items that Enemy has
+     * @param healthPoints the number of health points of Enemy
+     * @param weapon       weapon of Enemy
+     */
     public Player(List<Item> inventory, int healthPoints, Weapon weapon) {
         super(inventory, healthPoints, weapon);
     }
 
+    /**
+     * Creates a new Enemy with default inventory, health points, and weapon.
+     */
     public Player() {
         super();
     }
 
-        //Adds an item to the Player's inventory. If it's a weapon, equip it.
-    public <T> void pickUp(T article){
-        if (article instanceof Weapon){
+    /**
+     * Adds an item to the Player's inventory. If it's a weapon, equip it.
+     *
+     * @param article Item or Weapon subclass
+     */
+    public <T> void pickUp(T article) {
+        if (article instanceof Weapon) {
             this.setWeapon((Weapon) article);
         }
-        if(article instanceof Item){
+        if (article instanceof Item) {
             this.getInventory().add((Item) article);
             // need to call hp restoration?
         }
     }
 
-        //If the method is called on a weapon, equip it. Else, remove it from the Player's inventory
-    public <T> void disposeItem(T article){
-        if (article instanceof Weapon){
+    /**
+     * If the method is called on a weapon, equip it. Else, remove it from the Player's inventory
+     *
+     * @param article Item or Weapon subclass
+     */
+    public <T> void disposeItem(T article) {
+        if (article instanceof Weapon) {
             this.setWeapon(null);
         }
-        if(article instanceof Item){
+        if (article instanceof Item) {
             this.getInventory().remove((Item) article);
         }
     }

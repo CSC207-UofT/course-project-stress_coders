77fd5996b35b0bc0fd5f17c203ec84ac07fa7f85
Devan Srinivasan <devan.srinivasan@mail.utoronto.ca>
2021-11-11 19:24:50 -0800

    Fixing bufs

--- src/main/entities/Tree.java
+++ src/main/entities/Tree.java
@@ -18,12 +18,21 @@ public class Tree extends Interactable implements Harvestable {
         super.addProperty(InteractableProperties.RES_DIST_NAME.name(), new Variable(25));
     }
 
+    /**
+     * Add to the players inventory, the new resources
+     * @param player
+     * @return
+     */
     public String harvest(Player player){
         if (!(this.isCompleted())) {
             int distribution = this.getProperty(InteractableProperties.RES_DIST_NAME.name()).getInteger();
             player.addInventory("wood", distribution);
             int storage = this.getProperty(InteractableProperties.RES_STORE_NAME.name()).getInteger();
             this.addProperty(InteractableProperties.RES_STORE_NAME.name(), new Variable(storage - distribution));
+            if (storage - distribution == 0) {
+                this.setCompleted(true);
+            }
+
             return "Added " + distribution + " wood to " + player.getId() + " inventory";
         } else{
             return "No more left!";

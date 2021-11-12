1ae82109fb06264bdc9022dd8a5a95fa7d82293b
Devan Srinivasan <devan.srinivasan@mail.utoronto.ca>
2021-11-11 18:05:29 -0800

    Added an inventory to player

--- src/main/entities/Player.java
+++ src/main/entities/Player.java
@@ -3,10 +3,15 @@ package entities;
 import entities.Character;
 import entities.interfaces.ThrowableTarget;
 
+import java.util.HashMap;
+import java.util.List;
+
 /*
 The player character hold their inventory and their stats and handle how those change
  */
 public class Player extends Character implements ThrowableTarget {
+    private HashMap<String, List<Interactable>> inventory = new HashMap<String, List<Interactable>>();
+
     public Player(String id) {
         super(id);
     }
@@ -20,5 +25,9 @@ public class Player extends Character implements ThrowableTarget {
         } else {
             return "Your damage has decreased by " + damageDone + ", you have " + this.getHealthPoints() + " health points";
         }
+    }
+
+    public void addInventory(String name, List<Interactable> resources){
+        this.inventory.put(name, resources);
     }
 }

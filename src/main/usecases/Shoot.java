d8c84bc91a18145325fc0352e160f286eddd5054
MichelleChernyi <michelle.chernyi@mail.utoronto.ca>
2021-11-13 11:43:23 -0500

    fixed shoot command to check that character didn't run out of ammo

--- src/main/usecases/Shoot.java
+++ src/main/usecases/Shoot.java
@@ -5,9 +5,15 @@ import entities.interfaces.ThrowableTarget;
 
 import java.util.HashMap;
 
-/*
-I used a modified version of the Throw code, this might not be working
- */
+/**
+Shoot command, shoot an obj to a given target.
+The chance of hitting should be dictated by hitProbability property
+Shot objs must have a damage property
+
+If the shot obj hits:
+An enemy should be damaged by the damage of the shootableWeapon
+
+ **/
 
 public class Shoot extends Command{
 
@@ -17,22 +23,28 @@ public class Shoot extends Command{
         if(args.get(shotObject) instanceof ShootableWeapon && args.get(target) instanceof ThrowableTarget) {
             ThrowableTarget throwTarget = (ThrowableTarget) args.get(target);
             ShootableWeapon shot = (ShootableWeapon) args.get(shotObject);
-            shot.spendAmmo(1);
+            if(shot.getAmmoCount() > 0) {
+                shot.spendAmmo(1);
 
-            float hitProbVar = shot.getHitProb();
-            int hitProb = Math.round(hitProbVar);
+                float hitProbVar = shot.getHitProb();
+                int hitProb = Math.round(hitProbVar);
 
-            if(hitProb / 100.0 > Math.random()){
-                return throwTarget.handleHit(args.get(shotObject));
+                if (hitProb / 100.0 > Math.random()) {
+                    return throwTarget.handleHit(args.get(shotObject));
+                }
+
+                return "It missed";
             }
-
-            return "It missed";
         }
 
         System.out.println(args.get(shotObject));
         if(!(args.get(shotObject) instanceof ShootableWeapon)){
-            return "You shoot throw that";
+            return "You cannot shoot that";
         }
+        if(((ShootableWeapon) args.get(shotObject)).getAmmoCount() == 0){
+            return "You're out of ammunition";
+        }
+
 
         return "You cannot shoot something at that";
     }

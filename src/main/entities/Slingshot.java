89fb678658ada87005703f129586e8505c2cd103
MichelleChernyi <michelle.chernyi@mail.utoronto.ca>
2021-11-13 21:21:33 -0500

    added more shootable weapons + small fixes

--- src/main/entities/Slingshot.java
+++ src/main/entities/Slingshot.java
@@ -1,18 +1,15 @@
 package entities;
 import java.lang.Math;
 
-/*
-A crossbow is a shootable weapon with damage that can vary from 0.8 times the damage value to 1.4 times the damage value
- */
 
-public class Crossbow extends ShootableWeapon{
+public class Slingshot extends ShootableWeapon{
 
     /**
      * constructs a CrossBow object
      * @param id the id of the crossbow
      * @param ammoCount the amount of ammo (arrows) the crossbow comes with
      */
-    public Crossbow(String id, int ammoCount) {
+    public Slingshot(String id, int ammoCount) {
         super(id);
         this.addAmmo(ammoCount);
     }
@@ -21,7 +18,7 @@ public class Crossbow extends ShootableWeapon{
      */
     @Override
     public void addHitProbability() {
-        super.addProperty(InteractableProperties.HIT_PROB.name(), new Variable(60));
+        super.addProperty(InteractableProperties.HIT_PROB.name(), new Variable(50));
     }
 
     /**
@@ -29,8 +26,10 @@ public class Crossbow extends ShootableWeapon{
      */
     @Override
     public void addWeight() {
-        int weight = 10 + (int)(Math.random() * (90 + 1));
+        int weight = 10 + (int)(Math.random() * (40 + 1));
         super.addProperty(InteractableProperties.WEIGHT.name(), new Variable(weight));
     }
 
 }
+
+

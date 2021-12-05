e423f9f11450c60434475f69313450bb5c682b1f
Riyan Ahmed <riyan.ahmed@outlook.com>
2021-12-05 00:14:52 -0500

    Updated LaunchCommand

--- src/main/interfaceadapters/commands/LaunchCommand.java
+++ src/main/interfaceadapters/commands/LaunchCommand.java
@@ -2,12 +2,18 @@ package interfaceadapters.commands;
 
 import entities.Interactable;
 import usecases.Command;
+import usecases.Launch;
 
 import java.util.HashMap;
 
 public class LaunchCommand extends Command {
+
+    Launch launch;
+
+    public LaunchCommand() {this.launch = new Launch(); }
+
     @Override
     public String execute(HashMap<String, Interactable> args) {
-        return null;
+        return this.launch.playInLaunch(args);
     }
 }

985efe6fc89274e8e35e1370bac4936bcec428ba
shehzaad777 <75758176+shehzaad777@users.noreply.github.com>
2021-12-06 00:50:15 -0500

    bug fix for weapons

--- src/main/entities/interfaces/Target.java
+++ src/main/entities/interfaces/Target.java
@@ -3,9 +3,5 @@ package entities.interfaces;
 import entities.Interactable;
 
 public interface Target {
-    /**
-     * Depicts if something can get hit by a shootable
-     * See ShootableWeapon
-     */
-    String shotAt(Interactable shootable);
+
 }
